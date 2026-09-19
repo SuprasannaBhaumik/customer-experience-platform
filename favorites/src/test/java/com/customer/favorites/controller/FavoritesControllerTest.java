cc4606dc81f67dc9af271a5c5139edd79893ec76
SuprasannaBhaumik <suprasanna.bhaumik86@gmail.com>
2026-09-19 22:21:18 +0530

    day-07 | added repository tests, apierror and globalexceptionhandler | suprasanna

--- favorites/src/test/java/com/customer/favorites/controller/FavoritesControllerTest.java
+++ favorites/src/test/java/com/customer/favorites/controller/FavoritesControllerTest.java
@@ -1,4 +1,12 @@
 package com.customer.favorites.controller;
 
 public class FavoritesControllerTest {
+
+    /* POST returns 201
+duplicate returns 409
+missing favorite returns 404
+DELETE returns 204 */
+
+
+
 }
--- favorites/src/test/java/com/customer/favorites/service/FavoritesServiceTest.java
+++ favorites/src/test/java/com/customer/favorites/service/FavoritesServiceTest.java
@@ -1,4 +1,24 @@
 package com.customer.favorites.service;
 
 public class FavoritesServiceTest {
+
+
+    /* shouldAddFavorite
+    
+    
+    shouldRejectDuplicateFavorite
+    
+    
+    shouldReturnFavoritesForCustomer
+    
+    
+    shouldReturnSingleFavorite
+    
+    
+    shouldThrowWhenFavoriteMissing
+    
+    
+    shouldDeleteFavorite */
+
+
 }
