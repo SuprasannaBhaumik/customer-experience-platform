49497f83a2e49831d8734b66490fc6749ec2a31b
SuprasannaBhaumik <suprasanna.bhaumik86@gmail.com>
2026-09-20 20:22:56 +0530

    day-07 | added controller test | suprasanna

--- favorites/src/main/java/com/customer/favorites/controller/FavoritesController.java
+++ favorites/src/main/java/com/customer/favorites/controller/FavoritesController.java
@@ -10,31 +10,33 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.customer.favorites.dto.FavoritesDTO;
 import com.customer.favorites.service.FavoritesService;
 
 @RestController
+@RequestMapping("/api/v1/customers/{customerId}/favorites")
 public class FavoritesController {
 
     @Autowired 
     private FavoritesService service;
 
-    @PostMapping("/customers/{customerId}/favorites/{productId}")
+    @PostMapping("/{productId}")
     public ResponseEntity<FavoritesDTO> saveFavoriteProduct(@PathVariable UUID customerId, @PathVariable UUID productId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.saveFavorite(customerId, productId));
     }
 
-    @GetMapping("/customers/{customerId}/favorites")
+    @GetMapping
     public ResponseEntity<List<FavoritesDTO>> getFavoritesProductList(@PathVariable UUID customerId) {
         return ResponseEntity.status(HttpStatus.OK).body(service.listAllFavoritesOfACustomer(customerId));
     }
 
-    @DeleteMapping("/customers/{customerId}/favorites/{productId}")
-    public ResponseEntity<String> deleteFavoriteProduct(@PathVariable UUID customerId, @PathVariable UUID productId) {
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> deleteFavoriteProduct(@PathVariable UUID customerId, @PathVariable UUID productId) {
         service.deleteFavoriteOfCustomerByProduct(customerId, productId);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("success");
+        return ResponseEntity.noContent().build();
     }
 
 
--- favorites/src/test/java/com/customer/favorites/controller/FavoritesControllerTest.java
+++ favorites/src/test/java/com/customer/favorites/controller/FavoritesControllerTest.java
@@ -1,5 +1,26 @@
 package com.customer.favorites.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.customer.favorites.dto.FavoritesDTO;
+import com.customer.favorites.service.FavoritesService;
+
+@WebMvcTest 
 public class FavoritesControllerTest {
 
     /* POST returns 201
@@ -8,5 +29,66 @@ missing favorite returns 404
 DELETE returns 204 */
 
 
+    @Autowired 
+    private MockMvc mockMvc;
+
+    @MockitoBean 
+    private FavoritesService service;
+
+    @Test 
+    public void shouldCreateFavorite() throws Exception {
+
+        UUID customerId = UUID.randomUUID();
+        UUID productId = UUID.randomUUID();
+
+        FavoritesDTO favoritesDTO = 
+            new FavoritesDTO(
+                12L, 
+                customerId, 
+                productId, 
+                Instant.now()
+            );
+
+        when(
+            service
+            .saveFavorite(
+                any(UUID.class), 
+                any(UUID.class)
+            )
+        ).thenReturn(favoritesDTO);
+
+        mockMvc
+        .perform(
+            post(
+                "/api/v1/customers/{customerId}/favorites/{productId}", 
+                customerId, 
+                productId
+            )            
+            .contentType(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(status().isCreated());
+
+    }
+
+
+    @Test 
+    public void deleteSuccessfull() throws Exception {
+        //DELETE returns 204
+
+        UUID customerId = UUID.randomUUID();
+        UUID productId = UUID.randomUUID();
+
+        mockMvc
+        .perform(delete(
+            "/api/v1/customers/{customerId}/favorites/{productId}",
+                customerId,
+                productId
+            )
+        )
+        .andExpect(status().isNoContent());
+
+        verify(service).deleteFavoriteOfCustomerByProduct(customerId, productId);
+
+    }
 
 }
